637d6dd115442fd897b438ee2d6ee5b5fc1f187a
brml <brml@itu.dk>
2024-11-02 14:18:30 +0100

    Change to non-lazy list instead of streams and convert from cents to dollars

--- src/main/java/dk/acto/blackdragon/service/ModelTransformerImpl.java
+++ src/main/java/dk/acto/blackdragon/service/ModelTransformerImpl.java
@@ -2,7 +2,8 @@ package dk.acto.blackdragon.service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.stream.Stream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import dk.acto.blackdragon.model.Model;
 import dk.acto.blackdragon.model.Stats;
@@ -13,27 +14,30 @@ public class ModelTransformerImpl implements ModelTransformer<Model, Stats> {
     @Override
     public Stats transform(List<Model> model) {
         var statsBuilder = Stats.builder();
-        Stream<Long> ids = model.toJavaStream().map(m -> m.getId());
-        Stream<BigDecimal> costs = model.toJavaStream().map(m -> BigDecimal.valueOf(((double)m.getCost())/100.0));
-        Stream<BigInteger> inventories = model.toJavaStream().map(m -> BigInteger.valueOf(m.getInventory()));
+        List<Long> ids = model.map(m -> m.getId());
+        List<BigDecimal> costsInCents = model.map(m -> BigDecimal.valueOf(m.getCost()));
+        List<BigInteger> inventories = model.map(m -> BigInteger.valueOf(m.getInventory()));
         
-        int evenIds = ids.filter(id -> id % 2 == 0).toList().size();
-        int oddIds = ids.filter(id -> id % 2 == 1).toList().size();
+        int evenIds = ids.filter(id -> id % 2 == 0).length();
+        int oddIds = ids.filter(id -> id % 2 == 1).length();
 
-        statsBuilder.evenIds(BigInteger.valueOf(evenIds));
-        statsBuilder.oddIds(BigInteger.valueOf(oddIds));
+        statsBuilder
+            .evenIds(BigInteger.valueOf(evenIds))
+            .oddIds(BigInteger.valueOf(oddIds));
 
-        BigDecimal bigSum = costs.reduce(BigDecimal.ZERO, BigDecimal::add);
-        statsBuilder.meanCost(bigSum.divide(BigDecimal.valueOf(costs.toList().size())));
+        BigDecimal meanCost = costsInCents
+            .fold(BigDecimal.ZERO, BigDecimal::add)
+            .divide(BigDecimal.valueOf(costsInCents.toList().size()));
 
+        statsBuilder.meanCost(meanCost.divide(BigDecimal.valueOf(100)));
 
         BigDecimal weightInventory = model
             .map(m -> BigDecimal.valueOf(m.getInventory()).multiply(BigDecimal.valueOf(m.getWeight())))
             .fold(BigDecimal.ZERO, BigDecimal::add);
 
-        statsBuilder.weightedInventory(weightInventory);
+        statsBuilder.weightedInventory(weightInventory); 
 
-        statsBuilder.totalInventory(inventories.reduce(BigInteger.ZERO, BigInteger::add));
+        statsBuilder.totalInventory(inventories.fold(BigInteger.ZERO, BigInteger::add));
 
         return statsBuilder.build();
     }
