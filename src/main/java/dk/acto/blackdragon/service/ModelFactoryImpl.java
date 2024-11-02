d17c4582a7aa75f498eb439ab071f7aa3866e9f3
brml <brml@itu.dk>
2024-11-02 11:22:46 +0100

    Parse csv using basic methods

---
+++ src/main/java/dk/acto/blackdragon/service/ModelFactoryImpl.java
@@ -0,0 +1,26 @@
+package dk.acto.blackdragon.service;
+
+import io.vavr.collection.List;
+
+import dk.acto.blackdragon.model.Model;
+
+public class ModelFactoryImpl implements ModelFactory<Model> {
+
+    @Override
+    public List<Model> parse(String string) {
+        String[] input = string.split("\n");
+        List<Model> result = List.empty();
+        for (int i = 1; i < input.length; i ++) {
+            var line = input[i].split(", ");
+            var modelBuilder = Model.builder()
+                .id(Long.parseLong(line[0]))
+                .weight(Double.parseDouble(line[1]))
+                .cost(Integer.parseInt(line[2]))
+                .inventory(Long.parseLong(line[3]));
+
+            result.append(modelBuilder.build());
+        }
+        return result;
+    }
+    
+}
